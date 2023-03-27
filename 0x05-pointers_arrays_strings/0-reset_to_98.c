902aef92ee5040b8f2c3d12f4611d9610e3e6d8e
yusifhuseini <yusifhuseini@gmail.com>
2023-03-27 13:44:27 -0700

    Add function to update int value to 98 using a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Changes the value that it is
+ * pointing to, to 98
+ * @n - the paramater
+ * return - void
+ * Code by - yusifhuseini
+**/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
