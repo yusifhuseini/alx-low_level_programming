7455d9cc19358edc64857a40abd89b359e39cec3
yusifhuseini <yusifhuseini@gmail.com>
2023-03-27 14:37:43 -0700

    Implement function to swap values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,10 +2,10 @@
 
 /**
  * swap_int - exchanges the values of two integers
- * @a - the integer that will be swapped with @b
- * @b - the integer that will be swapped with @a
+ * @a: the integer that will be swapped with @b
+ * @b: the integer that will be swapped with @a
  * Code by - yusifhuseini
-**/
+ */
 
 void swap_int(int *a, int *b)
 {
