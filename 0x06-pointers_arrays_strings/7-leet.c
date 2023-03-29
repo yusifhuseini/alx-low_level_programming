ced79a5b49cca1c3b9daf448f5d1913e52250ce0
yusifhuseini <yusifhuseini@gmail.com>
2023-03-29 15:41:01 -0700

    Add function to encode a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string using 1337 code
+ * @s: pointer to the input string
+ * Return: a pointer to the resulting encoded string
+ * Code by - yusifhuseini
+ */
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
