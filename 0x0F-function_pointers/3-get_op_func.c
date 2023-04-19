35b0f4a3100a5ba050ca7513503520c9e081f70b
yusifhuseini <yusifhuseini@gmail.com>
2023-04-19 09:06:37 -0700

    Implement function to select appropriate operation based on user input

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func -  func select to implement user command
+ * @s: input char point arg
+ * Return: func pointer or NULL
+ * Code by - yusifhuseini
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
