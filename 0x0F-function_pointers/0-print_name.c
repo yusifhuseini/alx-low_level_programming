e6d36e80aa070595bd23ccd40a57c076fe1bce39
yusifhuseini <yusifhuseini@gmail.com>
2023-04-19 09:05:37 -0700

    Implement function to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name func
+ * @name: pointer - name - print
+ * @f: func print name
+ * Return: void
+ * Code by - yusifhuseini
+ */
+void print_name(char *name, void (*f)(char *name))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
