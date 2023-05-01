889dbbf6656c43125d56c51c4ad01e8e728dd6b8
Huseini Yusif <yusifhuseini@gmail.com>
2023-05-01 07:35:17 -0700

    Add function to delete the node at index index of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse linked list
+ * @head: head list
+ * Code by - yusifhuseini
+ * Return: first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p;
+	listint_t *n;
+
+	p = NULL;
+	n = NULL;
+
+	while (*head != NULL)
+	{
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+	}
+
+	*head = p;
+	return (*head);
+}
